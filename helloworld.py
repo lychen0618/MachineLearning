c33d5f27ff9f20a796416a6feebe6fd745ce9680
lychen0618 <412369718@qq.com>
2020-10-27 14:15:03 +0800

    add a few things from cs229_python

--- helloworld.py
+++ helloworld.py
@@ -1,1 +1,15 @@
 print("Hello World!")
+
+# 格式化
+print("he.（upper)".upper())  # 还有其他很多函数：strip()
+print('Old school formatting: %.2f' % (1.358))
+
+# 连接
+print("Hello" + " lychen" + str(618))
+print(f'{print}(print a function)')
+print(f'{type(229)}(print a type)')
+
+# list
+list_1=["one","two","three"]
+list_1.append(4)
+print(list_1)
